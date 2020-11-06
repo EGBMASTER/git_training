d3b6ca3c79d4b6cb03ba92dad709151d72213d33
EGBMASTER <enric.guillot.7e4@itb.cat>
2020-11-06 11:13:10 +0100

    bon dia

---
+++ If.java
@@ -0,0 +1,12 @@
+public class If{
+public static void main(String[] args){
+Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+int nota = scanner.nextInt();
+if(nota<10 && nota>5)
+	System.out.println("nota aprobada")
+if else(nota<0 && nota>10
+	System.out.println("nota no valida")
+else
+	System.out println("nota suspesa")
+}
+}
